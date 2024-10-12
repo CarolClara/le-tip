eec66ed0e574fa785ea2998cce522685ed09c7c1
Caroline Clara de Souza <caroline@onfly.com.br>
2024-10-12 17:10:37 -0300

    Ajusta estrutura dos arquivos

---
+++ src/components/custom/index.ts
@@ -0,0 +1,4 @@
+import CurrencyButtonToggle from "@/components/custom/buttons/CurrencyButtonToggle.vue";
+import MonetaryInput from "@/components/custom/inputs/MonetaryInput.vue";
+
+export { CurrencyButtonToggle, MonetaryInput };
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import CurrencyButtonToggle from "@/components/CurrencyButtonToggle.vue";
-import MonetaryInput from "@/components/MonetaryInput.vue";
-
-export { CurrencyButtonToggle, MonetaryInput };
